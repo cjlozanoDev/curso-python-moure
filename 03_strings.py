fe8a556a6dba0a5adce6a09bc0a23d9f9dae963e
cjlozanoDev <carlosjlp@gmail.com>
2023-01-09 17:59:46 +0100

    class progress 3:14:00

---
+++ 03_strings.py
@@ -0,0 +1,66 @@
+### Strings ###
+
+my_string = "Mi String"
+my_other_string = "Mi Otro String"
+
+print(len(my_string))
+print(len(my_other_string))
+
+print(my_string + " " + my_other_string)
+
+my_new_line_string = "Este es un String\ncon salto de línea"
+print(my_new_line_string)
+
+my_tab_string = "\tEste es un String con tabulación"
+print(my_tab_string)
+
+my_tab_string = "\\tEste es un String \\n escapado"
+print(my_tab_string)
+
+# Formateo
+
+name, surname, age = "Carlos Javier", "Lozano", 36
+
+print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
+print("Mi nombre es %s %s y mi edad es %d" %(name, surname, age))
+print(f"Mi nombre es {name} {surname} y mi edad es {age}")
+
+# Desempaquetado de caracteres
+
+language = "python"
+a, b, c, d, e, f = language
+
+print(a)
+print(e)
+
+
+# División
+
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
+
+language_slice = language[0:6:2]
+print(language_slice)
+
+
+# Reverse
+
+reversed_language = language[::-1]
+print(reversed_language)
+
+
+# Funciones del sistema
+
+print(language.capitalize())
+print(language.upper())
+print(language.count("t"))
+print(language.isnumeric())
+print(language.lower())
+print(language.upper().isupper())
+print(language.startswith("py"))
---
+++ 04_lists.py
@@ -0,0 +1,15 @@
+### Lists ###
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [33, 36, 24, 62, 30, 30, 17]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [36, 1.73, "Carlos Javier", "Lozano"]
+
+print(type(my_other_list))
